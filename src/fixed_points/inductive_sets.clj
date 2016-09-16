75377d63f071f321ec4daee0b379ad90e78d2550
Fredokun <fredokun@github.com>
2016-09-16 16:05:02 +0200

    one important proof pasted

--- src/fixed_points/inductive_sets.clj
+++ src/fixed_points/inductive_sets.clj
@@ -90,6 +90,29 @@
 ;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:defined</span>","value":":defined"},{"type":"html","content":"<span class='clj-keyword'>:term</span>","value":":term"},{"type":"html","content":"<span class='clj-symbol'>inductive-set</span>","value":"inductive-set"}],"value":"[:defined :term inductive-set]"}
 ;; <=
 
+;; @@
+(defthm closed-set-lower-bound
+  "If `Q` is an `R`-closed set, then the inductive
+  set defined on `R` is included in `Q`."
+  [[T :type] [R (rules T)] [Q (set T)]]
+  (==> (closed-set T R Q)
+       (subset T (inductive-set T R) Q)))
+;; @@
+
+;; @@
+(proof closed-set-lower-bound
+    :script
+    (assume [H (closed-set T R Q)]
+       (have a (subset T (inductive-set T R) Q)
+           :by ((pset/intersections-lower-bound T (lambda [E (set T)]
+                                                        (closed-set T R E)))
+                Q H))
+       (qed a)))
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:qed</span>","value":":qed"},{"type":"html","content":"<span class='clj-symbol'>closed-set-lower-bound</span>","value":"closed-set-lower-bound"}],"value":"[:qed closed-set-lower-bound]"}
+;; <=
+
 ;; **
 ;;; A property of this set is that it is itself @@R@@-closed.
 ;; **
@@ -100,6 +123,10 @@
   [[T :type] [R (rules T)]]
   (closed-set T R (inductive-set T R)))
 ;; @@
+;; ->
+;;; [Warning] redefinition as theorem:  closed-inductive-set
+;;; 
+;; <-
 ;; =>
 ;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:declared</span>","value":":declared"},{"type":"html","content":"<span class='clj-keyword'>:theorem</span>","value":":theorem"},{"type":"html","content":"<span class='clj-symbol'>closed-inductive-set</span>","value":"closed-inductive-set"}],"value":"[:declared :theorem closed-inductive-set]"}
 ;; <=
@@ -114,18 +141,62 @@
       (assume [Y (set T)
                HY (closed-set T R Y)]
          (have a (subset T (inductive-set T R) Y)
-               :by ((pset/intersections-lower-bound T (lambda [E (set T)]
-                                                        (closed-set T R E)))
-                    Y HY))
+               :by ((closed-set-lower-bound T R Y) HY))
          (have b (subset T X Y)
                :by ((set/subset-trans T X (inductive-set T R) Y)
                     H2 a))
          (have c (elem T y Y) :by (HY X y H1 b))
-         
-         )))
-
-         
+         (have d (forall [Y (set T)]
+                    (==> (closed-set T R Y)
+                         (elem T y Y)))
+               :discharge [Y HY c]))
+       (qed d)))      
 ;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:qed</span>","value":":qed"},{"type":"html","content":"<span class='clj-symbol'>closed-inductive-set</span>","value":"closed-inductive-set"}],"value":"[:qed closed-inductive-set]"}
+;; <=
+
+;; @@
+(defthm inductive-prop
+  ""
+  [[T :type] [R (rules T)] [P (==> T :type)]]
+  (==> (subset T (inductive-set T R)
+               (lambda [x T]
+                  (and (elem T x (inductive-set T R))
+                       (P x))))
+       (forall [x T]
+          (==> (elem T x (inductive-set T R))
+               (P x)))))
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:declared</span>","value":":declared"},{"type":"html","content":"<span class='clj-keyword'>:theorem</span>","value":":theorem"},{"type":"html","content":"<span class='clj-symbol'>inductive-prop</span>","value":"inductive-prop"}],"value":"[:declared :theorem inductive-prop]"}
+;; <=
+
+;; @@
+(proof inductive-prop
+   :script
+   )
+;; @@
+
+;; @@
+(defthm rule-induction
+  "The principle of rule induction."
+  [[T :type] [R (rules T)] [P (==> T :type)]]
+  (==> (forall [X (set T)]
+           (forall [y T]
+               (==> (R X y)
+                    (forall [x T]
+                       (==> (elem T x X)
+                            (and (elem T x (inductive-set T R))
+                                 (P x))))
+                    (P y))))
+       (forall [x T]
+          (==> (elem T x (inductive-set T R))
+               (P x)))))
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:declared</span>","value":":declared"},{"type":"html","content":"<span class='clj-keyword'>:theorem</span>","value":":theorem"},{"type":"html","content":"<span class='clj-symbol'>rule-induction</span>","value":"rule-induction"}],"value":"[:declared :theorem rule-induction]"}
+;; <=
 
 ;; @@
 
